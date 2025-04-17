0c03285bea7a2b4506a8bc20f449138692929208
Eltrach <aeltrach@gmail.com>
2025-04-17 12:58:12 +0100

    Refactor app.js for improved readability and consistency; standardize code formatting and enhance session secret generation logic.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const config = require('./config')
-const { setEnvDataSync } = require('./utils/env.util')
-const { generateRandomString } = require('./utils/random.util')
+const config = require('./config');
+const { setEnvDataSync } = require('./utils/env.util');
+const { generateRandomString } = require('./utils/random.util');
 const path = require('path');
 const serve = require('koa-static');
 const render = require('koa-ejs');
@@ -10,17 +10,10 @@ const koaBody = require('koa-body');
 const session = require('koa-session');
 const Koa = require('koa');
 
-// Init Application
-
-if(!config.APP_USERNAME || !config.APP_PASSWORD){
-    console.log("You must first setup admin user. Run command -> npm run setup-admin-user")
-    process.exit(2)
-}
-
-if(!config.APP_SESSION_SECRET){
-    const randomString = generateRandomString()
-    setEnvDataSync(config.APP_DIR, { APP_SESSION_SECRET: randomString})
-    config.APP_SESSION_SECRET = randomString
+if (!config.APP_SESSION_SECRET) {
+  const randomString = generateRandomString();
+  setEnvDataSync(config.APP_DIR, { APP_SESSION_SECRET: randomString });
+  config.APP_SESSION_SECRET = randomString;
 }
 
 // Create App Instance
@@ -37,17 +30,17 @@ app.use(koaBody());
 
 app.use(serve(path.join(__dirname, 'public')));
 
-const router = require("./routes");
+const router = require('./routes');
 app.use(router.routes());
 
 render(app, {
-    root: path.join(__dirname, 'views'),
-    layout: 'base',
-    viewExt: 'html',
-    cache: false,
-    debug: false
+  root: path.join(__dirname, 'views'),
+  layout: 'base',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
 });
 
-app.listen(config.PORT, config.HOST, ()=>{
-    console.log(`Application started at http://${config.HOST}:${config.PORT}`)
-})
+app.listen(config.PORT, config.HOST, () => {
+  console.log(`Application started at http://${config.HOST}:${config.PORT}`);
+});
